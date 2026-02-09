f8eacaa31cf2ffe891cf696628dcc34316debb94
Aanaastaasiaaa <bolobanav6203214@yandex.ru>
2026-02-09 21:54:07 +0300

    Update ConsoleApplication6.cpp

--- ConsoleApplication6.cpp
+++ ConsoleApplication6.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
+
 using namespace std;
 
 const double pi = acos(-1.0);
@@ -10,33 +12,42 @@ double fuc(int x)
     return x * fuc(x - 1);
 }
 
-double fun(double x, int maxn=1000)
+double fun(double x, int maxn = 1000)
 {
     double sc = 2.0 / sqrt(pi);
     double sum = 0;
     int n = 0;
-    double f = 0;
+    double prev = -1;
 
     while (n < maxn)
     {
-        double  a1 = pow(-1, n);
+        double a1 = pow(-1, n);
         double a2 = pow(x, 2 * n + 1);
         double a3 = fuc(n);
         double a4 = 2 * n + 1;
-        double res = a1 * a2 / (a3 * a4);
+        double term = a1 * a2 / (a3 * a4);
 
-        sum += res;
-        if (sum==f)
+        sum += term;
+
+        if (sum == prev) {
             break;
-        f = sum;
+        }
+
+        prev = sum;
         n++;
     }
-    return sum*sc;
+
+    return sum * sc;
 }
 
 int main()
 {
     double x[] = { 0.5, 1.0, 5.0, 10.0 };
-    for(int i=0;i<4;i++)
-        cout<<fun(x[i]) << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << "x = " << x[i] ;
+        cout << scientific << setprecision(8);
+        cout << "  erf(x) = " << erf(x[i]) << endl;
+    }
+
+    return 0;
 }
