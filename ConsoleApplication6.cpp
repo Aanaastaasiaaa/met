ad13a5885efe251bbf2daae4df3d025adbb8bde7
Aanaastaasiaaa <bolobanav6203214@yandex.ru>
2026-02-11 09:13:28 +0300

    Update ConsoleApplication6.cpp

--- ConsoleApplication6.cpp
+++ ConsoleApplication6.cpp
@@ -4,23 +4,23 @@
 
 using namespace std;
 
-const double pi = acos(-1.0);
-
 double fuc(int x)
 {
     if (x == 1 || x == 0) return 1;
     return x * fuc(x - 1);
 }
 
-double fun(double x, int maxn = 1000)
+double fun(double x, int maxn = 100)
 {
+    const double pi = acos(-1.0);
     double sc = 2.0 / sqrt(pi);
-    double sum = 0;
+    double sum = 0.0;
     int n = 0;
     double prev = -1;
 
-    while (n < maxn)
+    while (!(n > maxn))
     {
+
         double a1 = pow(-1, n);
         double a2 = pow(x, 2 * n + 1);
         double a3 = fuc(n);
@@ -42,11 +42,12 @@ double fun(double x, int maxn = 1000)
 
 int main()
 {
+    setlocale(LC_ALL, "RUS");
     double x[] = { 0.5, 1.0, 5.0, 10.0 };
     for (int i = 0; i < 4; i++) {
-        cout << "x = " << x[i] ;
-        cout << scientific << setprecision(8);
-        cout << "  erf(x) = " << erf(x[i]) << endl;
+        cout << "x = " << x[i];
+        cout << fixed << setprecision(12);
+        cout << "  erf(x) = " << erf(x[i]) << " fun(x) = "<<fun(x[i])<<" разница =  "<<fabs(fun(x[i])- erf(x[i])) << endl;
     }
 
     return 0;
