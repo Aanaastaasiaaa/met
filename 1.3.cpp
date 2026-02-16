a48492e83d1caa65d04c652cb0fed55132c6e411
Aanaastaasiaaa <bolobanav6203214@yandex.ru>
2026-02-16 22:13:59 +0300

    Update 1.3.cpp

--- 1.3.cpp
+++ 1.3.cpp
@@ -1,66 +1,62 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 #include <iomanip>
 using namespace std;
 
 const double pi = acos(-1.0);
+const double pi2 = pi * pi;
+const double pi4 = pi2 * pi2;
 
-double met1(double eps, int& n)
-{
+double method1(double eps, int& terms) {
     double sum = 0.0;
-    n = 1;
+    terms = 0;
 
-    while (true)
-    {
-        double term = 1.0 / (n * n + 1);
+    for (int n = 1; ; n++) {
+        double term = 1.0 / (n * n + 1.0);
+        double prev = sum;
         sum += term;
+        terms++;
 
-        if (term < eps)
+        if (sum == prev) {
             break;
-
-        n++;
+        }
     }
 
     return sum;
 }
 
-double met2(double eps, int& n)
-{
-    double sum = 0.0;
-    n = 1;
+double method2(double eps, int& terms) {
+    double sum = pow(pi, 2) / 6.0 - pow(pi, 4) / 90.0;
+    terms = 0;
 
-    while (true)
-    {
-        double term = 1.0 / (n * n * (n * n + 1));
+    for (int n = 1; ; n++) {
+        double term = 1.0 / (n * n * n * n * n * n * (n * n + 1.0));
+        double prev = sum;
         sum += term;
+        terms++;
 
-        if (term < eps)
+        if (sum == prev || term < eps) {
             break;
-
-        n++;
+        }
     }
 
-    return (pi * pi) / 6.0 - sum;
+    return sum;
 }
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "rus");
     cout << fixed << setprecision(12);
 
     double eps = 1e-10;
+    int n1, n2, n3;
 
-    int n1, n2;
-    double s1 = met1(eps, n1);
-    double s2 = met2(eps, n2);
+    cout << "Исходный ряд:" << endl;
+    double s1 = method1(eps, n1);
+    cout << "Сумма = " << s1 << " членов = " << n1 << endl;
 
-    double exact = (pi * cosh(pi) / sinh(pi) - 1) / 2;
-
-    cout << "исходный метод:" << endl;
-    cout << "сумма = " << s1 <<" членов = " << n1 << endl;
-
-    cout << "преобразованный метод:" << endl;
-    cout << "сумма = " << s2 <<" членов = " << n2 << endl;
+    cout << "Преобразованный ряд:" << endl;
+    double s2 = method2(eps, n2);
+    cout << "Сумма = " << s2 << " операций = " << n2 + 2  << endl;
 
     return 0;
 }
