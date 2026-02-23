c4464ac2325f5252d9c7574093221147ead814fa
Aanaastaasiaaa <bolobanav6203214@yandex.ru>
2026-02-23 13:03:32 +0300

    Update 1.3.cpp

--- 1.3.cpp
+++ 1.3.cpp
@@ -4,59 +4,79 @@
 using namespace std;
 
 const double pi = acos(-1.0);
-const double pi2 = pi * pi;
-const double pi4 = pi2 * pi2;
+const double e = 1e-10;
 
-double method1(double eps, int& terms) {
+double met1(double e) {
     double sum = 0.0;
-    terms = 0;
+    int n = 1;
+    double term;
 
-    for (int n = 1; ; n++) {
-        double term = 1.0 / (n * n + 1.0);
-        double prev = sum;
+    while (true) {
+        term = 1.0 / (n * n + 1.0);
+        if (term < e) break;
         sum += term;
-        terms++;
-
-        if (sum == prev) {
-            break;
-        }
+        n++;
     }
-
     return sum;
 }
 
-double method2(double eps, int& terms) {
-    double sum = pow(pi, 2) / 6.0 - pow(pi, 4) / 90.0;
-    terms = 0;
-
-    for (int n = 1; ; n++) {
-        double term = 1.0 / (n * n * n * n * n * n * (n * n + 1.0));
-        double prev = sum;
+double kl1(double e) {
+    double sum = 0.0;
+    int n = 1;
+    double term;
+    int kl = 0;
+    while (true) {
+        term = 1.0 / (n * n + 1);
+        kl++;
+        if (term < e) break;
         sum += term;
-        terms++;
-
-        if (sum == prev || term < eps) {
-            break;
-        }
+        n++;
     }
+    return kl;
+}
 
+
+double met2(double e) {
+
+    double sum = 0.0;
+    int n = 1;
+    double term;
+    while (true) {
+        term = (1.0 / (n * n)) - (1.0 / (n * n * n * n + n * n));
+        if (abs(term) < e) break;
+        sum += term;
+        n++;
+    }
     return sum;
 }
+
+double kl2(double e) {
+
+    double sum = 0.0;
+    int n = 1;
+    double term;
+    int kl = 0;
+
+    while (true) {
+        term = (1.0 / (n * n)) - (1.0 / (n * n * n * n + n * n));
+        kl++;
+        if (abs(term) < e) break;
+        sum += term;
+        n++;
+    }
+    return kl;
+}
+
 
 int main() {
     setlocale(LC_ALL, "rus");
-    cout << fixed << setprecision(12);
 
-    double eps = 1e-10;
-    int n1, n2, n3;
+    cout << fixed << setprecision(10);
 
-    cout << "Исходный ряд:" << endl;
-    double s1 = method1(eps, n1);
-    cout << "Сумма = " << s1 << " членов = " << n1 << endl;
+    cout << "Сумма исходного ряда: " << met1(e) << endl;
 
-    cout << "Преобразованный ряд:" << endl;
-    double s2 = method2(eps, n2);
-    cout << "Сумма = " << s2 << " операций = " << n2 + 2  << endl;
+    cout << "Сумма преобразованного ряда: " << met2(e) << endl;
 
-    return 0;
+    cout << "Преобразованный ряд сходится в " << kl1(e) / kl2(e) << " раз быстрее" << endl;
+
 }
