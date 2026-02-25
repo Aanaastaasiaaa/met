5386060df89819ec5bb69bde079f0bda5db78a1b
Aanaastaasiaaa <bolobanav6203214@yandex.ru>
2026-02-25 09:10:13 +0300

    Update 1.2.cpp

--- 1.2.cpp
+++ 1.2.cpp
@@ -3,39 +3,36 @@
 #include <iomanip>
 using namespace std;
 
-double s_direct(double x, double eps, int& terms1, int& terms2, double& sumtime) {
-    double sum1 = 0.0, sum2 = 0.0;
-    terms1 = 0, terms2 = 0;
-    const double time = 0.0005; 
-    sumtime = 0.0;
-
-    for (int k = 1; ; k++) {
-        double t = 1.0 / (k * (k + x));
-        sum1 += t;
-        terms1++;
-        sumtime += time;
-        if (fabs(t) < eps) break;
-    }
-
-    for (long long k = 1; ; k++) {
-        double t = 1.0 / (k * (k - x));
-        sum2 += t;
-        terms2++;
-        sumtime += time;
-        if (fabs(t) < eps) break;
-    }
-
-    return sum1 - sum2;
-}
-
-double s_fast(double x, double eps, int& terms, double& sumtime) {
-    double sum = 0.0;
-    terms = 0;
+double s(double x, double eps, int& terms, double& sumtime) {
+    double sum=0.0;
+    terms = 1;
     const double time = 0.0005;
     sumtime = 0.0;
 
     for (int k = 1; ; k++) {
-        double t = (2.0 * x) / (k * (k * k - x * x));
+        double sum1 = 1.0 / sqrt(k * k * k + x);
+        double sum2 = 1.0 / sqrt(k * k * k - x);
+        sumtime += time;
+        double s = sum1 - sum2;
+        if (fabs(s) < eps) break;
+        terms++;
+        sumtime += time;
+        sum += s;
+    }
+
+    return sum;
+}
+
+double s_fast(double x, double eps, int& terms, double& sumtime) {
+    double sum = 0.0;
+    terms = 1;
+    const double time = 0.0005;
+    sumtime = 0.0;
+
+    for (int k = 1; ; k++) {
+        double a = sqrt(k * k * k + x);
+        double b = sqrt(k * k * k - x);
+        double t = (2.0 * x) / (a*b*(a+b));
         sum += t;
         terms++;
         sumtime += time;
@@ -53,15 +50,15 @@ int main() {
     double x2 = 0.999999999;
     double eps = 3e-8;
 
-    int terms1, terms2;
+    int terms1;
     double time;
-    double s1 = s_direct(x1, eps, terms1, terms2, time);
-    cout << "x = " << x1 << endl << " s(x) = " << s1 << " Всего членов: " << terms1 + terms2 << endl;
+    double s1 = s(x1, eps, terms1, time);
+    cout << "x = " << x1 << endl << " s(x) = " << s1 << " Всего членов: " << terms1  << endl;
     cout << "Время: " << time << " секунд" << endl;
-    int terms1_2, terms2_2;
+    int terms1_2;
     double time_2;
-    double s2 = s_direct(x2, eps, terms1_2, terms2_2, time_2);
-    cout << "x = " << setprecision(9) << x2 << " s(x) = " << s2 << " Всего членов: " << terms1_2 + terms2_2 << endl;
+    double s2 = s(x2, eps, terms1_2, time_2);
+    cout << "x = " << setprecision(9) << x2 << " s(x) = " << s2 << " Всего членов: " << terms1_2 << endl;
     cout << "Время: " << time_2 << " секунд" << endl;
 
     cout << " Ускоренное вычисление" << endl;
@@ -77,7 +74,7 @@ int main() {
     cout << "Время: " << time_fast2 << " секунд" << endl;
 
     cout << "Сравнение результатов" << endl;
-    cout << "Для x = 0.5.Разность: " << fabs(s1 - s1_fast) << endl;
+    cout << "Для x = 0.5. Разность: " << fabs(s1 - s1_fast) << endl;
 
     cout << "Для x = 0.999999999. Разность: " << fabs(s2 - s2_fast) << endl;
 
